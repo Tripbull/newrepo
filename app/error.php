0640febcbe64c3141fbc6a03daa8b33b1e474cdf
tripbull <robert.garlope@tabluu.com>
2015-07-10 16:02:19 +0800

    remove

--- app/error.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <title>403 Forbidden</title>
-</head>
-<body style="margin:0 auto; text-align:center;">
-    <h3 style="padding-top:100px;">This page is not available</h3>
-</body>
-</html>
