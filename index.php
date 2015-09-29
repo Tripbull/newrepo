8988bbe0b10d90d8507f5d6fafa86d3f6ce624b7
tripbull <robert.garlope@tabluu.com>
2015-09-29 17:28:06 +0800

    remove hashtag

--- index.php
+++ index.php
@@ -8,11 +8,11 @@ $_SESSION['session']=$ur_session;
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="google-site-verification" content="EQez1wVJd5ruzADIL1OQrBMh391ORWp8Nzfkpkhpso8" />
-<title>Camrally - Your photo campaign can change the world!</title>
-<meta name="title" content="Camrally is an awesome photo campaign service to help you rally advocates for anything!">
+<meta name="google-site-verification" content="EQez1wVJd5ruzADIL1OQrBMh391ORWp8Nzfkpkhpso8">
+<title>Camrally - Your indispensible advocate driven campaign marketing platform!</title>
+<meta name="title" content="Camrally is campaign marketing platform that helps you leverage on opinion advocates, ambassadors & user generated content">
 <meta name="keywords" content="fund raising, photo contests, brand awareness, new product launch, fanbase building, interest groups, event promotions and marketing promotions">
-<meta name="description" content="Create a photo or selfie campaign in 10 mins & rally advocates for any cause, brand, product, service, anything at all!">
+<meta name="description" content="Create a photo video campaign in 10 mins & rally advocates for any cause, brand, product, service, anything at all!">
 <link href="css/bootstrap.css" rel="stylesheet" media="all">
 <link href="css/style.css" rel="stylesheet" media="all">
 <link rel="stylesheet" href="css/flexslider.css" media="all">
@@ -41,7 +41,7 @@ $_SESSION['session']=$ur_session;
   <div class="container">
       <div class="qrcode-content" style="margin-top:30px;">
         <div class="cust-post"> 
-       <h4 class="plan-title title1">Create your own photo campaign in 10 mins & change the world. It’s free!</h4>
+       <h4 class="plan-title title1">Leverage the power of advocates for your campaigns today!</h4>
            <div class="clear" style="padding:1em 0;"></div>
             <div class="tblcontainer">
               <div class="row">
@@ -51,15 +51,15 @@ $_SESSION['session']=$ur_session;
               </div>
             </div>
           <div class="text-left div-p cam-selfie">
-            <p>Camrally is an online service for creating photo & selfie campaigns. The name Camrally stands for Camera, Campaign & Rallying for advocates! Start a campaign today to better your community, your society and your country. Be empowered to change the world, one snap at a time!</p> 
+            <p>The name Camrally stands for camera, campaign & rallying for advocates! Advocates join your campaign rally by submitting relevant videos & images. They are also encouraged to submit their own videos & photos! Leverage on these "self appointed" ambassadors, user generated content & crowd behaviour to bring your message to the right audience!</p> 
             <div class="clear" style="padding:0.7em 0;"></div>
-            <p>Camrally is also great for fundraising, fanbase building, event promotion, photo contest, new product launch and marketing promotion. Create an interest group or use Camrally “just for fun”. It’s like having Causes.com, Pinterest & Instagram combined! Start a campaign and rally for advocates now. With Camrally, you are limited only by your own creativity!</p>
+            <p>Camrally is great for promoting any cause, fundraising, fanbase building, event promotion, photo/video contest, new product launch and marketing promotion. Camrally is your indispensible tool for campaign marketing success!</p>
           </div>
           <div class="clear" style="padding:1em 0;"></div>
           <div class="tblcontainer">
             <div class="row">
               <div class="text-center">
-                <div class="link"><a href="#" class="signupbtn">I wanna change the world!</a></div>
+                <div class="link"><a href="#" class="signupbtn">I want advocates for my campaign!</a></div>
               </div>
             </div>
           </div>
@@ -73,7 +73,7 @@ $_SESSION['session']=$ur_session;
 <div class="container">
   <div class="qrcode-content">
     <div class="cust-post">
-       <h4 class="plan-title title4">Camrally + Selfies = Online Mass Rally for Advocates!</h4>
+       <h4 class="plan-title title4">Camrally + Advocates = Online Mass Rally!</h4>
        <div class="clear" style="padding:1em 0;"></div>
       <!-- <div class="hidden-xs"> -->
         <div class="tblcontainer">
@@ -86,10 +86,10 @@ $_SESSION['session']=$ur_session;
         <div class="cam-fail">
           <ul>
             <li>If you don’t have a website, you don’t need one now.</li>
-            <li>All posts link to the social media accounts of your advocates.</li>
-      <li>All photo posts in one page!</li>
-            <li>Display relevant images of your campaign.</li>
-            <li>Your custom Camrally page URL!</li>
+            <li>All posts from your advocates link to their social media accounts.</li>
+      	    <li>All your advocates' posts in one page!</li>
+            <li>Display relevant videos & images of your campaign.</li>
+            <li>Get your custom Camrally page URL!</li>
             <li>Get followers & many more features!</li>
           </ul>
         </div>
@@ -114,12 +114,11 @@ $_SESSION['session']=$ur_session;
             <div class="poster-right check-list-ul">
               <ul>
                 <li>In-built viral mechanism.</li>
-                <li>Your participants don’t have to download any app.</li>
+                <li>Your advocates don’t have to download any app.</li>
                 <li>Discovery to participation in one click!</li>
                 <li>Works on all devices and browsers.</li>
                 <li>Clear &amp; consistent campaign message at all touch points.</li>
                 <li>Pro-actively ask advocates to share to Facebook &amp; Twitter.</li>
-                <li>Get &amp; promote a mini link that redirects to your Camrally campaign page.</li>
               </ul>
             </div>
           </div>
@@ -128,7 +127,7 @@ $_SESSION['session']=$ur_session;
         <div class="tblcontainer">
           <div class="row">
               <div class="text-center">
-                <div class="link"><a href="#" class="signupbtn">I wanna change the world!</a></div>
+                <div class="link"><a href="#" class="signupbtn">I want advocates for my campaign!</a></div>
               </div>
           </div>
         </div>
@@ -138,13 +137,15 @@ $_SESSION['session']=$ur_session;
 </div>
 
 <div class="qrcode-wrap secondrow">
+<!--
 <div class="clear" style="padding:1em 0;"></div>
+
 <div class="container">
   <div class="qrcode-content">
     <div class="cust-post">
        <h4 class="plan-title title3">No hashtags. No problems.</h4>
        <div class="clear" style="padding:1em 0;"></div>
-      <!-- <div class="hidden-xs"> -->
+    
         <div class="tblcontainer">
           <div class="row">
             <div class="gallery">
@@ -157,14 +158,14 @@ $_SESSION['session']=$ur_session;
             <li>No fear of hashtags spam</li>
             <li>No fear of typo errors with long hashtags</li>
             <li>No fear that your audience can’t remember complicated hashtags</li>
-			<li>No need elaborate instructions on which hashtags to use</li>
+	    <li>No need elaborate instructions on which hashtags to use</li>
           </ul>
         </div>
     </div>
   </div>
 </div>  
 </div>
-
+-->
 <div class="qrcode-wrap" style="background-color:#1DAFEC;">
 <div class="clear" style="padding:1em 0;"></div>
 <div class="container">
@@ -206,7 +207,7 @@ $_SESSION['session']=$ur_session;
   <div class="plan">
     <div class="row">
       <div class="col-sm-9">
-        <h2><span><img src="images/soundicon.png" alt="icon"></span>Remember, your photo campaign can change the world!</h2>
+        <h2><span><img src="images/soundicon.png" alt="icon"></span>Discover how Camrally can take your campaign marketing to the next level!</h2>
       </div>
       <div class="col-sm-3"><a class="pull-right signupbtn" href="#">Sign Up</a></div>
     </div>
