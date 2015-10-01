15d9200cea3c0a5a195697453115c00792a605aa
tripbull <robert.garlope@tabluu.com>
2015-10-01 15:42:10 +0800

    bugs

--- app/js/dashboard.js
+++ app/js/dashboard.js
@@ -1892,7 +1892,7 @@ $(document).ready(function(){
 				//if($(this).index() == 1){
 					//defaultSetup = 2;
 					//$( ":mobile-pagecontainer" ).pagecontainer( "change", "setup.html",{});
-				if($(this).index() > 4){
+				if($(this).index() > 5){
 				}else{	
 					curClick = $(this).index();
 					showHideMenuProfile(curClick);
